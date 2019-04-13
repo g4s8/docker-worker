0f60509cf65e559ec0cef71eb29f076c26bcb642
Kirill <g4s8.public@gmail.com>
2019-04-13 16:09:57 +0300

    #1 - example

---
+++ example/docker.tf
@@ -0,0 +1,30 @@
+
+# AWS region to run
+variable "aws_region" {
+  default = "us-east-2"
+}
+
+# Public key
+variable "ssh_pub_key" {}
+
+# AWS profile
+variable "aws_profile" {
+  default = "default"
+}
+
+# Using AWS provider
+provider "aws" {
+  region = "${var.aws_region}"
+  profile = "${var.aws_profile}"
+}
+
+module "docker-builder" {
+  source = "github.com/g4s8/docker-worker"
+  tag_name = "docker-builder"
+  ssh_pub_key = "${var.ssh_pub_key}"
+  aws_zone = "us-east-2a"
+}
+
+output "public-ip" {
+  value = "${module.docker-builder.ip}"
+}
